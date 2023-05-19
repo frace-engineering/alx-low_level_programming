c50a6c1287539cf3ae55134ff7e7dbfa8b03d0c3
Ugwu Friday <fugwungene@gmail.com>
2023-05-19 19:42:05 +0000

    Add advanced task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alwats 0 (Success)
+ */
+int main(void)
+{
+	int comb;
+	int comb2;
+	int comb3;
+
+	comb = 0;
+	while (comb < 8)
+	{
+		comb2 = 1;
+		while (comb2 < 9)
+		{
+			comb3 = 2;
+			while (comb3 < 10)
+			{
+				if ((comb < comb2) && (comb2 < comb3))
+				{	
+					putchar(comb + '0');
+					putchar(comb2 + '0');
+					putchar(comb3 + '0');
+					if ((comb != 7) || (comb2 != 8) || (comb3 != 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				 }
+				comb3++;
+			 }
+			comb2++;
+		}
+		comb++;
+	}
+	putchar('\n');
+	return (0);
+}
