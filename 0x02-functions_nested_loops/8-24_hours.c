6a211c1be04c94e5ec75446117e2ec8e1dd81348
Ugwu Friday <fugwungene@gmail.com>
2023-05-24 13:28:48 +0000

    Modify counter

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 3; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			for (c = 0; c < 10; c++)
+			for (c = 0; c < 6; c++)
 			{
 				for (d = 0; d < 10; d++)
 				{
