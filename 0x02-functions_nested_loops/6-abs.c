25fe41621c4f71b20d6bdd40b8b90da9ae3a6c9e
Ugwu Friday <fugwungene@gmail.com>
2023-05-24 00:00:10 +0000

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - comput the absolut value of a number
+ *
+ * @n: input number
+ *
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
