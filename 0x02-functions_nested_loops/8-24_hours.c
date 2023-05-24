434403d1d0997f1cdff45242b51204e59094fc85
Ugwu Friday <fugwungene@gmail.com>
2023-05-24 13:08:05 +0000

    Include return statement

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0; a < 3; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 4; b++)
+		for (b = 0; b < 10; b++)
 		{
-			for (c = 0; c < 6; c++)
+			for (c = 0; c < 10; c++)
 			{
 				for (d = 0; d < 10; d++)
 				{
@@ -27,6 +27,8 @@ void jack_bauer(void)
 					_putchar(c + '0');
 					_putchar(d + '0');
 					_putchar('\n');
+					if (a == 2 && b == 3 && c == 5 && d == 9)
+						return;
 				}
 			}
 		}
