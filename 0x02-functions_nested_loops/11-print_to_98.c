461973421f016316fa79ededf327241cb06df3ce
Ugwu Friday <fugwungene@gmail.com>
2023-05-24 14:02:57 +0000

    Modify code
    Print last number "98"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,23 @@ void print_to_98(int n)
 			n++;
 		}
 	}
-	if (n < 98)
+	if (n <= 98)
 	{
 		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
+		printf("%d", 98);
 	}
-	else if (n > 98)
+	else if (n >= 98)
 	{
 		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+		printf("%d", 98);
 	}
 	else if (n == 98)
 		printf("%d", 98);
