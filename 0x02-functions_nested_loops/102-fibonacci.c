f7899ad7c5d6562b3d88ef76141e86a7fe94c9da
Ugwu Friday <fugwungene@gmail.com>
2023-05-25 04:46:02 +0000

    Add task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int fib1, fib2, i, temp, fib;
+
+	fib1 = 1;
+	fib2 = 2;
+	printf("%d, %d, ", fib1, fib2);
+	fib = 0;
+	for (i = 0; i < 50; i++)
+	{
+		fib = fib1 + fib2;
+		printf("%d, ", fib);
+		temp = fib2;
+		fib2 = fib;
+		fib1 = temp;
+	}
+	printf("%d", fib);
+	printf("\n");
+	return (0);
+}
+
+
+
