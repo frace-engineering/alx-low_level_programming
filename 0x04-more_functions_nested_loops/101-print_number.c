0fd3df7b9de26dc8191a0f3c00e2adf537dbb41b
Ugwu Friday <fugwungene@gmail.com>
2023-05-26 04:44:21 +0000

    Correct the typo in function name

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input number
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else if (i == n)
+	{
+		i = n;
+	}
+	if (i > 10)
+	{
+		_putchar((i / 10) + '0');
+	}
+	_putchar((i % 10) + '0');
+}
