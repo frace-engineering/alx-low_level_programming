9cb7a91575fbc070ae0eb9cbf1cb367ea6a2af6b
Ugwu Friday <fugwungene@gmail.com>
2023-05-31 20:26:51 +0000

    Modify code
    Correct typo in task 2
    Fix bug in task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,9 +17,8 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1 == s2)
 			return (0);
-		else if (s1 < s2)
-			return (-1);
-		return (1);
+		else if (s1 != s2)
+			return (*s1 - *s2);
 	}
 	return (0);
 }
