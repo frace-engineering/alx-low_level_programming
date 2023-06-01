4fd0c33962a1e3b9136d312ec8a0ff435f350c2b
Ugwu Friday <fugwungene@gmail.com>
2023-06-01 01:52:22 +0000

    Add modified code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,12 +13,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; ((s1[i] != '\0') && (s2[i] != '\0')); i++)
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (i = 0; s2[i] != '\0'; i++)
 	{
-		if (s1 == s2)
-			return (0);
-		else if (s1 != s2)
+		if (s1 != s2)
+		{
 			return (*s1 - *s2);
+		}
 	}
 	return (0);
 }
