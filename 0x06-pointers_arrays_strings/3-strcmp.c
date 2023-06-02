ab2f307d3e6b2ff769bbf0ed95881e2914392c77
Ugwu Friday <fugwungene@gmail.com>
2023-06-02 10:30:23 +0000

    Fix bug

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s2[j] != '\0'; j++)
+	for (i = 0; s2[i] != '\0'; i++)
 	{
 		if (s1 != s2)
 		{
