241a09e2a31cd956f0d7cfcd9f3a652706f486d3
Ugwu Friday <fugwungene@gmail.com>
2023-06-02 17:16:23 +0000

    Add task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * rot13 - encodes strings using rot13
+ * @str: string
+ * Return: pointer to str
+ */
+int _strlen(char *str)
+{
+	int i;
+	int len;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len += 1;
+	}
+	return (len);
+}
+
+char *rot13(char *str)
+{
+	char letter;
+	int i;
+	int length;
+
+
+	length = _strlen(str);
+	printf("%d\n", length);
+	for (i = 0; i < length; i++)
+	{
+		letter = str[i];
+		if ((letter >= 65 && letter <= 90) || (letter >=97 && letter <= 122))
+		{
+			str[i] -= 13;
+		}
+	}
+	return (str);
+}
