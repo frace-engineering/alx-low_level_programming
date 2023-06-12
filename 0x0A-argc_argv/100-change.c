87f8efabca239abdeb41939b438c69db2315c9de
Ugwu Friday <fugwungene@gmail.com>
2023-06-12 18:21:02 +0000

    Refactor the code
    use subtraction instead of division and modulusy

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,71 +4,49 @@
 
 
 /**
+ * change - print the minimum number of coin to make change for
+ * any amount of money
+ * @cent: value of money to given
+ * Return: Always 0(Success)
+ */
+int change(int cent)
+{
+	int coin[5] = {25, 10, 5, 2, 1};
+	int i, j = 0;
+	int coinlist[5] = {0};
+
+	for (i = 0; i < 5; i++)
+	{
+		while (cent >= coin[i])
+		{
+			cent -= coin[i];
+			coinlist[j++] = coin[i];
+		}
+	}
+	for (i = 0; i < j; i++)
+	{
+		printf("%d ", coinlist[i]);
+	}
+	printf("\n");
+	exit(EXIT_SUCCESS);
+}
+/**
  * main - entry point
  * @argc: argument count
  * @argv: pointer to array of argument
  * Return: 0(Success)
  */
 int main(int argc, char *argv[])
-{	
-	char change[] = {25, 10, 5, 2, 1};
-	int i;
-	int b;
-
+{
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	if (atoi(argv[1]) <= 0)
 	{
-		printf("%d\n", 0);
-		return (0);
+		printf("%d", 0);
 	}
-	b = atoi(argv[1]);
-	for (i = 0; i < 5; i++)
-	{
-		if (b / change[i] > 0)
-		{
-			printf("%d ", change[i]);
-		}
-		if (b % change[i] != 0)
-			b = b % change[i];
-		for (i = 1; i <= 5; i++)
-		{
-			if (b / change[i] != 0)
-			{
-				printf("%d ", change[i]);
-			}
-			if (b % change[i] != 0)
-				b = b % change[i];
-			for (i = 2; i < 5; i++)
-			{
-				if (b / change[i] != 0)
-				{
-					printf("%d ", change[i]);
-				}
-				if (b % change[i] != 0)
-					b = b % change[i];
-				for (i = 3; i < 5; i++)
-				{
-					if (b / change[i] != 0)
-					{
-						printf("%d ", change[i]);
-					}
-					if (b % change[i] != 0)
-						b = b % change[i];
-					for (i = 4; i < 5; i++)
-					{
-						if (b / change[i] != 0)
-						{
-							printf("%d ", change[i]);
-						}
-					}
-				}
-			}
-		}
-	}
-	printf("\n");
+	change(atoi(argv[1]));
 	exit(EXIT_SUCCESS);
 }
