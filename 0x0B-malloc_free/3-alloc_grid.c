4399f41e9bc6973079d7158d88a833df095dc3e6
Ugwu Friday <fugwungene@gmail.com>
2023-06-14 12:24:00 +0000

    Fix bug in code
    Return Null if inner malloc fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,18 +18,22 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(sizeof(int) * height);
+	ptr = malloc(sizeof(int *) * height);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(sizeof(int) * width);
+		ptr[i] = malloc(sizeof(int *) * width);
 		if (ptr[i])
 		{
 			for (j = 0; j < height; j++)
 			{
 				ptr[i][j] = 0;	
 			}
+		}
+		else
+		{
+			return (NULL);
 		}
 	}
 	return (ptr);
