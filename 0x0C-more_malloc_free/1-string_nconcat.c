8fd8c8724f4829a4fdaf8530747bffd584a99819
Ugwu Friday <fugwungene@gmail.com>
2023-06-15 16:45:44 +0000

    Add mandatory task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * _strlen - calculate string length
+ * @s: input string
+ * Return: length of string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i, count;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count += 1;
+	return (count);
+}
+/**
+ * string_nconcat - concatenates 2 strings
+ * @s1: first input string
+ * @s2: second input string
+ * @n: number of char to be copied from s2 to s1
+ * Return: pointer to new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int total;
+	unsigned int i, j;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	total = _strlen(s1) + _strlen(s2);
+	ptr = malloc(sizeof(*ptr) * (total));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; ((j < n) && (s2[j] != '\0')); j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
+}
