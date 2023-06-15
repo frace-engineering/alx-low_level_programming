3544b851d5f2cf21390e9d9ede60f5f433b75b24
Ugwu Friday <fugwungene@gmail.com>
2023-06-15 19:25:32 +0000

    Add mandatory task 2
    use malloc to implement calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+
+/**
+ * _calloc - allocates memory for an array using calloc
+ * @:nmemb: array members
+ * @size: size of the array
+ * Return: pointer to the array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int **ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(*ptr) *nmemb);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = malloc(sizeof(*ptr) * size);
+		if (ptr[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (*ptr);
+}
