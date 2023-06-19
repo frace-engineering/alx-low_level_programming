8fed8f401c8acf9ba40394628e470676a9b64f8d
Ugwu Friday <fugwungene@gmail.com>
2023-06-19 15:37:00 +0000

    Modify macro  functionwq

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 
+
 /*
  * main - entry point
  * Return: 0 (Success)
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
 #ifndef ABSMACRO_H
 #define ABSMACRO_H
 
-#define ABS(x) ((x) * (-1))
+#define ABS(x) ( (x < 0) ? ((x) * (-1)): ((x) * (1)))
+
 
 #endif /* ABSMACRO_H */
