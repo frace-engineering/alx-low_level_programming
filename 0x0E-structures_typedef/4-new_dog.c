e0cad1afeb7089288d2c7c5353ab838da50a9408
Ugwu Friday <fugwungene@gmail.com>
2023-06-19 17:56:32 +0000

    Modify code
    allocate memory for the strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 
 /**
@@ -12,15 +13,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog;
-	dog_t *ptr = &new_dog;
+	dog_t *new_dog;
 
-	ptr = malloc(sizeof(dog_t) + 1);
-	if (ptr == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	new_dog->name = malloc(sizeof(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
 
-	return (ptr);
+	new_dog->owner = malloc(sizeof(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
+
+
+	new_dog->age = age;
+
+	return (new_dog);
 }
