8bc0e0d236d14259f35ebc0b66153a4211afa404
Ugwu Friday <fugwungene@gmail.com>
2023-06-20 08:58:22 +0000

    Allocate memory for structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,6 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(d));
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
