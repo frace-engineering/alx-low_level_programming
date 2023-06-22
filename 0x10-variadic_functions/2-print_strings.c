c414f8ed80c186b9361a96170f81e208f509af48
Ugwu Friday <fugwungene@gmail.com>
2023-06-22 20:56:36 +0000

    Add task two

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+ * print_strings - print strings passed to function
+ * @separator: string separators of the numbers
+ * @n: number of arguments to the function
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list pt;
+	char *str;
+
+	if (n == 0)
+		return;
+	va_start(pt, n);
+
+	for (i = 0; i < n; i++)
+	{ 
+		str = va_arg(pt, char *);
+		if (str == NULL)
+			printf("(nul)");
+		printf("%s", str);
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(pt);
+	printf("\n");
+}
