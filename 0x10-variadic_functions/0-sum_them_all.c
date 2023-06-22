449ba31eaed0d9c93154d16e894adf45810fbd4b
Ugwu Friday <fugwungene@gmail.com>
2023-06-22 20:38:30 +0000

    Separate declaration and assignment of sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,13 +12,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned int i;
-        int sum = 0;
-
+        int sum;
+       
 	if (n == 0)
 		return (0);
 
 	va_start(ptr, n);
 
+	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
