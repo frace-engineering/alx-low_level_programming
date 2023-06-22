f294117df74a2d663ef1b946981820db38fc8868
Ugwu Friday <fugwungene@gmail.com>
2023-06-22 15:23:15 +0000

    Add task zeroy

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+
+/**
+ * sum_them_all - returns the sum of all the parameters
+ * @n: integer number
+ * Return: return the sum of the args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This file contains the description of the 0x10-variadic_functions project tasks.
