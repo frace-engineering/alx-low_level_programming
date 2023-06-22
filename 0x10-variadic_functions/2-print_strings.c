7822ae33859553b1317c2d138d1abbde0aca9e1d
Ugwu Friday <fugwungene@gmail.com>
2023-06-22 21:01:46 +0000

    Enclose if statement properly

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,9 +23,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{ 
 		str = va_arg(pt, char *);
 		if (str == NULL)
-			printf("(nul)");
+			printf("(nil)");
 		printf("%s", str);
-		if (i != (n - 1) && separator != NULL)
+		if ((i != (n - 1)) && (separator != NULL))
 		{
 			printf("%s", separator);
 		}
