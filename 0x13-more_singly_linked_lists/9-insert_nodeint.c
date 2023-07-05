69ed9f4e3948c6c765475d461497f5aae5e82049
Ugwu Friday <fugwungene@gmail.com>
2023-07-05 21:48:41 +0000

    Modify code
    Take care of case where idx = 0
    Take care of case where temp->next = NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,21 +17,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *nodeAtIndex;
 	listint_t *temp;
 
-	if (*head == NULL)
-		return (NULL);
 	temp = *head;
 	nodeAtIndex = malloc(sizeof(listint_t));
 	if (nodeAtIndex == NULL)
 		return (NULL);
 	nodeAtIndex->n = n;
+	if (*head == NULL)
+		return (NULL);
 	i = 1;
 	while (i < idx && temp->next != NULL)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+		{
+			temp = nodeAtIndex;
+			nodeAtIndex->next = NULL;
+		}
 		i++;
 	}
-	if (i < idx)
-		return (NULL);
+	if (idx == 0)
+	{
+		*head = nodeAtIndex;
+		nodeAtIndex->next = NULL;
+	}
 	nodeAtIndex->next = temp->next;
 	temp->next = nodeAtIndex;
 	return (nodeAtIndex);
