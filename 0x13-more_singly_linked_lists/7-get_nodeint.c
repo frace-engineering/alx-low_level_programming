34285a209537234105916a3f2f2797c5eb770e49
Ugwu Friday <fugwungene@gmail.com>
2023-07-06 11:53:34 +0000

    Fox bug in code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i;
 
-	temp = head;
 	if (head == NULL)
 		return (NULL);
+	temp = head;
 	i = 0;
-	while (temp != 0)
+	while (temp != NULL)
 	{
 		temp = temp->next;
 		i++;
