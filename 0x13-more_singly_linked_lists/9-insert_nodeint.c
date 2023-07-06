5b7cc193928037f699cd498521bf7a6ba99e1c5b
Ugwu Friday <fugwungene@gmail.com>
2023-07-06 21:54:00 +0000

    Fix bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < idx && temp->next != NULL)
 	{
 		temp = temp->next;
+		if (i == idx)
+		{
+			nodeAtIndex->next = temp->next;
+			temp->next = nodeAtIndex;
+		}
 		i++;
 	}
 	if (idx == 0)
