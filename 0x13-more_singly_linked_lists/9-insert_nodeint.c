92faab1d7e11248fb5c4d3b6c4c7446152c2eec6
Ugwu Friday <fugwungene@gmail.com>
2023-07-06 21:28:41 +0000

    Iterate till end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	i = 1;
 	temp = *head;
-	while (i < idx && temp->next != NULL)
+	while (i < idx && temp != NULL)
 	{
 		temp = temp->next;
 		i++;
