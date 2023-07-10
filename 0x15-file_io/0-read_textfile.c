762261b5e29df4a4b0a5d7ab3cfdc87cfd3092f2
Ugwu Friday <fugwungene@gmail.com>
2023-07-10 17:46:57 +0000

    Modify code
    Use malloc to allocate memory for buff

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,22 +12,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fp, rd;
 	size_t wrt;
-	char buff[1024];
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
-	if (letters == 0)
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp < 0)
-		return (-1);
+	{
+		free(buff);
+		return (0);
+	}
 	rd = read(fp, buff, letters);
 	if (rd < 0)
 		return (-1);
 
 	wrt = write(1, buff, rd);
-	if (!wrt)
+	if (wrt < 0)
+	{
+		free(buff);
 		return (0);
+	}
+	free(buff);
 	close(fp);
 	return (wrt);
 }
