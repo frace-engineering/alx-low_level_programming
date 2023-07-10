520be6939c455ad05b7ec0c891bfdcf613a4f2d2
Ugwu Friday <fugwungene@gmail.com>
2023-07-10 20:47:00 +0000

    Add more check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,13 +38,15 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	wrt = write(fp, text_content, (len + 1));
-	if (wrt < 0)
+	if (text_content != NULL)
 	{
-		return (-1);
+		wrt = write(fp, text_content, (len + 1));
+
+		if (wrt < 0)
+		{
+			return (-1);
+		}
 	}
 	close(fp);
 	return (1);
 }
-
-
