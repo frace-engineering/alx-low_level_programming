278acf4fc4a97168a7f6c9fbf668415fed3fd3f4
Ugwu Friday <fugwungene@gmail.com>
2023-07-10 17:28:04 +0000

    Take care of case where size is 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,12 +14,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	size_t wrt;
 	char buff[1024];
 
-	if (filename == NULL)
+	if (filename == NULL || letters == NULL)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp < 0)
-		return (0);
+		return (-1);
 	rd = read(fp, buff, letters);
+	if (rd < 0)
+		return (-1);
 
 	wrt = write(1, buff, rd);
 	if (!wrt)
