4b1ffcd1f1b4ad218b06044257ddc41d189a663c
Ugwu Friday <fugwungene@gmail.com>
2023-07-12 04:26:37 +0000

    Modify code
    Refactor code lines
    Include a loop for write statement

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,42 +17,54 @@ int main(int argc, char *argv[])
 		return (0);
 	if (argc != 3)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file to", 28);
+		write(STDERR_FILENO, "Usage: cp file_from file to\n", 28);
+		free(buff);
 		exit(97);
 	}
 	if (argv[1] == NULL)
 	{
-		write(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE", 45);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buff);
 		exit(98);
 	}
 	fp = open(argv[1], O_RDONLY);
 	if (fp == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE", 45);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buff);
+		close(fp);
 		exit(98);
 	}
-	fp1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fp1 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fp1 == -1)
 	{
-		write(2, "Error: Can't write to NAME_OF_THE_FILE", 38);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buff);
+		close(fp);
+		close(fp1);
 		exit(99);
 	}
-	fp_rd = read(fp, buff, 1024);
+	while ((fp_rd = read(fp, buff, 1024)) > 0)
+	{
+		fp_wrt = write(fp1, buff, fp_rd);
+		if (fp_wrt == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buff);
+			close(fp);
+			close(fp1);
+			exit(98);
+		}
+	}
 	if (fp_rd == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE", 45);
-		exit(98);
-	}
-	fp_wrt = write(fp1, buff, 1024);
-	if (fp_wrt == -1)
-	{
-		write(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE", 45);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buff);
+		close(fp);
 		exit(98);
 	}
 	free(buff);
 	close(fp);
 	close(fp1);
-	return (fp_wrt);
+	exit(fp_wrt);
 }
-
-
