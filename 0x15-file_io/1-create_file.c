d7bca9792898529a87a6143ddcdd21f4be1f0184
Ugwu Friday <fugwungene@gmail.com>
2023-08-07 12:15:16 +0000

    Add task zero, task one and readme file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+
+/**
+ * _len - get the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+int _len(const char *s)
+{
+	int i;
+
+	if (s == NULL)
+		return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+/**
+ * create_file - creates a file and write to it
+ * @filename: pointer to the file name
+ * @text_content: pointer to the text content
+ *
+ * Return: 1 on success else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp, len, wrt;
+
+	if (filename == NULL)
+		return (-1);
+	len = _len(text_content);
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fp < 0)
+	{
+		close(fp);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		wrt = write(fp, text_content, len);
+
+		if (wrt == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+This file contains the description of the 0x15-file_io project tasks
+
+1. 0-read_textfile.c is a c source file that reads a file and prints it on the stdout
+2. 1-create_file.c is a c source file that creats a file
+3. 2-append_text_to_file.c is a c source file that append a text to a file
