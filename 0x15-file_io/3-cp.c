76f628d017d20296169287a8983dfbda5b747031
Ugwu F. B <fugwungene@gmail.com>
2023-08-08 22:48:44 +0000

    Add modified 3-cp.c file
    Had loose object error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,47 +12,50 @@ int write_fd(int fd, int fd1);
 
 int main(int argc, char *argv[])
 {
-	char *buff;
-	int fd, fd1, fd_wrt, cf, cf1;
+	int fd, fd1, cf, cf1, fd_wrt;
 
-	buff = malloc(sizeof(char) * 1024);
-	if (buff == NULL)
-		return (0);
 	if (argc != 3)
 	{
 		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-		free(buff);
 		exit(97);
 	}
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buff);
-		exit(98);
-	}
 	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+	if (fd < 0 || argv[1] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buff);
 		exit(98);
 	}
 	fd1 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buff);
 		exit(99);
 	}
 	fd_wrt = write_fd(fd, fd1);
+	if (fd_wrt == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	else if (fd_wrt == 2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		exit(99);
+	}
+
 	cf = close(fd);
 	if (cf < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 	cf1 = close(fd1);
 	if (cf1 < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-	free(buff);
-	return (fd_wrt);
+		exit(100);
+	}
+	return (0);
 }
 /**
  * write_fd - copy text from one file to the other
@@ -72,14 +75,12 @@ int write_fd(int fd, int fd1)
 		wrt_fd = write(fd1, buff, rd);
 		if (wrt_fd < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd1);
-			exit(98);
+			return (1);
 		}
 	}
 	if (rd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd);
-		exit(98);
+		return (2);
 	}
 	return (wrt_fd);
 }
