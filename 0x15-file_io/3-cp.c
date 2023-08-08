a04baf5b6707ccd16b652bc3db7222a64ca52ac6
Ugwu Friday <fugwungene@gmail.com>
2023-08-08 13:37:14 +0000

    Modify code in 3-cp.c file
    Refactore code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 
+int write_fd(int fd, int fd1);
 /**
  * main - entry point
  * @argc: number of command line arguments
@@ -8,10 +9,11 @@
  *
  * Return: count of characters copied
  */
+
 int main(int argc, char *argv[])
 {
 	char *buff;
-	int fp, fp1, fp_rd, fp_wrt, cf, cf1;
+	int fd, fd1, fd_wrt, cf, cf1;
 
 	buff = malloc(sizeof(char) * 1024);
 	if (buff == NULL)
@@ -28,40 +30,56 @@ int main(int argc, char *argv[])
 		free(buff);
 		exit(98);
 	}
-	fp = open(argv[1], O_RDONLY);
-	if (fp < 0)
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buff);
 		exit(98);
 	}
-	fp1 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	if (fp1 < 0)
+	fd1 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fd1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buff);
 		exit(99);
 	}
-	while ((fp_rd = read(fp, buff, 1024)) > 0)
+	fd_wrt = write_fd(fd, fd1);
+	cf = close(fd);
+	if (cf < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	cf1 = close(fd1);
+	if (cf1 < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+	free(buff);
+	return (fd_wrt);
+}
+/**
+ * write_fd - copy text from one file to the other
+ * @fd: file descriptor of file one
+ * @fd1: second file descriptor
+ *
+ * Return: number of characters written
+ */
+int write_fd(int fd, int fd1)
+{
+	char buff[1024];
+	int rd;
+	int wrt_fd;
+
+	while ((rd = read(fd, buff, 1024)) > 0)
 	{
-		fp_wrt = write(fp1, buff, fp_rd);
-		if (fp_wrt < 0)
+		wrt_fd = write(fd1, buff, rd);
+		if (wrt_fd < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buff);
+			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd1);
 			exit(98);
 		}
 	}
-	if (fp_rd < 0)
+	if (rd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buff);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd);
 		exit(98);
 	}
-	if ((cf = close(fp)) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
-	if ((cf1 = close(fp1)) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp1);
-	free(buff);
-	return (fp_wrt);
+	return (wrt_fd);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <elf.h>
+int write_fd(int fd, int fd1);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
