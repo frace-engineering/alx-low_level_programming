f7adbba5e11afdf0a800643db897b86ff9db82a3
Engr Ugwu <fugwungene@gmail.com>
2023-12-14 13:51:23 +0000

    Fix bug in code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,7 +15,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		low = 0;
 		high = size - 1;
-		while (low <= high || ((low + high) < size))
+		while (low <= high)
 		{
 			mid = (low + high) / 2;
 			printf("Searching in array: ");
