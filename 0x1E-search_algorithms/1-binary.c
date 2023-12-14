fb74dd922f15ecf4cb3bc8f9d2ada530d46d9ee8
Engr Ugwu <fugwungene@gmail.com>
2023-12-14 14:08:29 +0000

    Add a delimiter ","

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,27 +11,33 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t low, mid, high, i;
 
-	if (array != NULL)
+	if (array == NULL)
 	{
-		low = 0;
-		high = size - 1;
-		while (low <= high)
-		{
-			mid = (low + high) / 2;
-			printf("Searching in array: ");
-			for (i = low; i <= high; i++)
-			{
-				printf("%d ", array[i]);
-			}
-			printf("\n");
-			if (array[mid] == value)
-				return (mid);
-			else if (array[mid] < value)
-				low = mid + 1;
-			else
-				high = mid - 1;
-		}
 		return (-1);
+	}
+	low = 0;
+	high = size - 1;
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
 	}
 	return (-1);
 }
