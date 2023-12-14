3149a78110b872b927b362d2ba7add3da40b557c
Engr Ugwu <fugwungene@gmail.com>
2023-12-14 13:31:41 +0000

    Add task 1
    Add the binary search task

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+/**
+ * binary_search - Search through an array usinf binary search algorithm
+ * @array: pointer to an array
+ * @size: size of the array
+ * @value: elment to be searched for
+ *
+ * Return: index of the element else return -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low, mid, high, i;
+
+	if (array != NULL)
+	{
+		low = 0;
+		high = size - 1;
+		while (low <= high)
+		{
+			mid = (low + high) / 2;
+			printf("Searching in array: ");
+			for (i = low; i <= high; i++)
+			{
+				printf("%d ", array[i]);
+			}
+			printf("\n");
+			if (array[mid] == value)
+				return (mid);
+			else if (array[mid] < value)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+	}
+	return (-1);
+}
